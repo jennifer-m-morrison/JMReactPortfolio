8f614539051353fadcdff32913240893a24d5068
Jennifer Morrison <jmmorrison2@my.waketech.edu>
2024-07-10 10:58:54 -0400

    Formatting changes in experience section

--- src/Components/Experience.js
+++ src/Components/Experience.js
@@ -9,7 +9,7 @@ function Experience() {
             <Container className="experience-container px-0 py-5 mx-auto">
                 <h1 className="text-center mb-5">Professional Experience</h1>
                 <Row className="justify-content-center align-items-center g-4">
-                    <Col xs={9} md={10} lg={5} >
+                    <Col xs={10} md={11} lg={5} >
                         <h3 className="experience-text mb-2">XSAg.com</h3>
                         <h5 className="experience-text mb-4">Operations Analyst and Beta Tester</h5>
                         <ul>
@@ -18,7 +18,7 @@ function Experience() {
                         <li>Conducted routine user testing to implement new features and site enhancements</li>
                         </ul>
                     </Col>
-                    <Col xs={9} md={10} lg={5} >
+                    <Col xs={10} md={11} lg={5} >
                         <h3 className="experience-text mb-2">Rhone Poulenc Ag Company</h3>
                         <h5 className="experience-text mb-4">Market Research Analyst</h5>
                         <ul>
@@ -31,11 +31,11 @@ function Experience() {
                 <hr className="w-75 mx-auto" />
                 <h2 className="text-center py-4">Education</h2>
                 <Row className="justify-content-center align-items-evenly g-5">
-                    <Col xs={9} md={5} lg={4} className="experience-text d-flex flex-column">
+                    <Col xs={10} md={5} lg={4} className="experience-text d-flex flex-column">
                         <p><a href="https://www.waketech.edu/">Wake Tech Community College</a><br />Raleigh, NC</p>
                         <p>Pursuing A.A.S. Web Development</p>
                     </Col>
-                    <Col xs={9} md={5} lg={4} className="experience-text d-flex flex-column">
+                    <Col xs={10} md={5} lg={4} className="experience-text d-flex flex-column">
                         <p><a href="https://www.wm.edu"
                             target="_blank" rel="noopener noreferrer">College of William and Mary</a> <br />Williamsburg, VA</p>
                         <p>B.A. Economics</p>
