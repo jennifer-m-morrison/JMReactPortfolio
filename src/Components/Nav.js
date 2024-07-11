394a8822404e9bc1031514c28df0b0c6bee25b26
Jennifer Morrison <jmmorrison2@my.waketech.edu>
2024-07-10 20:37:40 -0400

    stylistic changes to hero.css and nav.js

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -21,10 +21,10 @@ function Navigation() {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ms-auto">
-                        <Nav.Link href="#about">About Me</Nav.Link>
-                        <Nav.Link href="#projects">Projects</Nav.Link>
-                        <Nav.Link href="#skills">Skills</Nav.Link>
-                        <Nav.Link href="#experience">Experience</Nav.Link>
+                        <Nav.Link className="px-md-3" href="#about">About Me</Nav.Link>
+                        <Nav.Link className="px-md-3" href="#projects">Projects</Nav.Link>
+                        <Nav.Link className="px-md-3" href="#skills">Skills</Nav.Link>
+                        <Nav.Link className="ps-md-3" href="#experience">Experience</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
